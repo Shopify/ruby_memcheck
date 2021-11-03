f77adf845bb3eb1ed83fb338548fb5af405ba69a
Peter Zhu <peter@peterzhu.ca>
2021-11-03 15:57:25 -0400

    Update documentation for Stack#skip?

--- lib/ruby_memcheck/stack.rb
+++ lib/ruby_memcheck/stack.rb
@@ -16,6 +16,8 @@ module RubyMemcheck
         fn = frame.fn
 
         if frame.in_ruby?
+          # If a stack from from the binary was encountered first, then this
+          # memory leak did not occur from Ruby
           unless in_binary
             # Skip this stack because it was called from Ruby
             return true if configuration.skipped_ruby_functions.any? { |r| r.match?(fn) }
@@ -23,7 +25,8 @@ module RubyMemcheck
         elsif frame.in_binary?
           in_binary = true
 
-          # Skip the Init function
+          # Skip the Init function because it is only ever called once, so
+          # leaks in it cannot cause memory bloat
           return true if fn == "Init_#{configuration.binary_name}"
         end
       end
