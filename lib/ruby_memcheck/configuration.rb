949c72f5f33960e9d8e4602ce020eb6e7a10351f
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2023-04-03 16:58:57 -0400

    Move all into ruby.supp

--- lib/ruby_memcheck/configuration.rb
+++ lib/ruby_memcheck/configuration.rb
@@ -111,7 +111,7 @@ module RubyMemcheck
       (0..3).reverse_each { |i| versions << full_ruby_version.split(".")[0, i].join(".") }
       versions << RUBY_ENGINE
 
-      files = versions.map do |version|
+      versions.map do |version|
         old_format_files = Dir[File.join(dir, "#{binary_name}_#{version}.supp")]
 
         unless old_format_files.empty?
@@ -121,8 +121,6 @@ module RubyMemcheck
 
         old_format_files + Dir[File.join(dir, "#{version}.supp")]
       end.flatten
-
-      files + Dir[File.join(dir, "rust.supp")]
     end
   end
 end
